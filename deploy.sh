02c85fee3f309a1e22cfadd824fe1d88852d8737
Allan Beaufour <allan@beaufour.dk>
2022-09-23 18:23:39 -0400

    chooses better name for the deployed cloud function

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo Generating requirements.txt
 poetry export > requirements.txt
-gcloud functions deploy python-http-function --gen2 --runtime=python39 --entry-point=calendar --trigger-http --allow-unauthenticated
+gcloud functions deploy calendar --gen2 --runtime=python39 --entry-point=calendar --trigger-http --allow-unauthenticated
 rm requirements.txt
