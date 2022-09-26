c328e4f82614cdc5c02c1a54f4b87eaf4a5c80e9
Allan Beaufour <allan@beaufour.dk>
2022-09-26 15:51:57 -0400

    adds Terraform config for deploying on custom domain

---
+++ terraform/variables.tf
@@ -0,0 +1,19 @@
+variable "region" {
+  type    = string
+  default = "us-east1"
+}
+
+variable "project_name" {
+  type    = string
+  default = "boonli-menu"
+}
+
+variable "dns_zone" {
+  type    = string
+  default = "vovhund-com"
+}
+
+variable "function_name" {
+  type    = string
+  default = "calendar"
+}
